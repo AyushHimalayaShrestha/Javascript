48c97efd0b16dcfdac846c3dd78de741ce0fb9dc
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-10 22:21:20 +0545

    regular expression

---
+++ ToDoList/script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    const taskList = document.getElementById('taskList');
+    const li = document.createElement('li');
+
+    li.innerHTML = `${taskText} <button onclick="removeTask(this)">X</button>`;
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    const taskList = document.getElementById('taskList');
+    taskList.removeChild(button.parentElement);
+}
