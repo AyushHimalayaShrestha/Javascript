2e8f28c044e8fe9cbab335a2c6305057e3dede7f
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-26 12:22:33 +0545

    scope

---
+++ scope.js
@@ -0,0 +1,26 @@
+function one(){
+    const username ='Ayush'
+
+    function two(){
+        const address ='Balaju'
+        console.log(username);
+    }
+    //console.log(address);
+
+    two()
+}
+one()
+
+if (true){
+    const username ='Ayush Himalaya Shrestha'
+    if (username == 'Ayush Himalaya Shrestha')
+    {
+        const address =' Balaju'
+        console.log(username+address);
+    }
+    // console.log(address);
+}
+// console.log(username);
+
+
+
