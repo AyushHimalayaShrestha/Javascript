287932b82d4646e8ece1651a3490f0abdc83407f
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-21 21:09:43 +0545

    function with default parameters

--- functionpractice.js
+++ functionpractice.js
@@ -2,3 +2,10 @@ function greet(name){
     return `Hello, ${name}`
 }
 console.log(greet("Ayush"));
+
+// Function with Default Parameters
+function add(a=5, b=10){
+    return a+b
+}
+console.log(add())
+console.log(add(5,8))
