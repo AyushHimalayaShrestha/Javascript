4863678127486e50bce4ccb46507033f6753e8b7
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-22 17:23:13 +0545

    For in Loop

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 // For Loop
 console.log('For Loop');
 for (let i=1; i<=5; i++){
-    console.log(`Iteration: ${i}`)
+    console.log(`Iteration: ${i}`);
 }
 
 // While Loop
@@ -13,17 +13,24 @@ while(j<=5){
 }
 
 // Do While Loop
-console.log('\nDo While Loop: ')
+console.log('\nDo While Loop: ');
 let k=1;
 do{
-    console.log(`Iteration: ${k}`)
+    console.log(`Iteration: ${k}`);
     k++;
 }
 while(k<=5);
 
 // For of Loop (Array Iteration)
-console.log('\nFor in Loop: ')
-const fruit =['Apple','Banana','Cherry','Mango','Guava']
-for(const fruits of fruit){
-    console.log(fruits);
+console.log('\nFor in Loop: ');
+const fruits =['Apple','Banana','Cherry','Mango','Guava']
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+// For in Loop (Object Iteration)
+console.log('\nFor in Loop');
+const person ={name:'Ayush Himalaya Shrestha', age:27, city:'Kathmandu'};
+for(const key in person){
+    console.log(`${key}:${person[key]}`);
 }
