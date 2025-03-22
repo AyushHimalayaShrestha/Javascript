78783f5ff71bc6d886db1bc0fd806370d0d400a4
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-22 17:05:14 +0545

    While Loop

--- loops.js
+++ loops.js
@@ -4,3 +4,10 @@ for (let i=1; i<=5; i++){
     console.log(`Iteration: ${i}`)
 }
 
+// While Loop
+console.log('\n While loop: ');
+let j=1;
+while(j<=5){
+    console.log(`Iteration: ${j}`);
+    j++;
+}
