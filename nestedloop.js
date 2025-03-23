d872f1a93af304bd0bfb59101feba707671060a5
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-23 21:22:32 +0545

    Iterating 2D Array

--- nestedloop.js
+++ nestedloop.js
@@ -17,3 +17,17 @@ for (let i=1; i<=size; i++){
     }
     console.log(row);
 }
+ 
+console.log("\t");
+
+//Iterating Over 2D Array
+let matrix =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+for(let i=0; i<matrix.length; i++){
+    for(let j=0; j<matrix[i].length; j++){
+        console.log(`Element at[${i}][${j}]: ${matrix[i][j]}`);
+    }
+}
