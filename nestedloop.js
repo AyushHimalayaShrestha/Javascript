6b0927bce7b9c42f28808d23ab77e82eca2cf5d0
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-23 21:15:43 +0545

    Multiplication Table

--- nestedloop.js
+++ nestedloop.js
@@ -5,3 +5,15 @@ for(let i =1; i<=n; i++){
     let stars="* ".repeat(i);
     console.log(space + stars);
 }
+
+console.log("\t");
+
+// Multiplication Table
+let size =5;
+for (let i=1; i<=size; i++){
+    let row = "";
+    for(let j=1; j<=size; j++){
+        row+= (i*j) + "\t";
+    }
+    console.log(row);
+}
